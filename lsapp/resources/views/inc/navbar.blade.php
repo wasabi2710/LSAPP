3ada5fe2cd27e0b7959c8784781fc7fde7df3ebe
WasabiTakeda <em.vannin.2001@gmail.com>
2023-08-16 19:52:24 +0700

    init mod

---
+++ lsapp/resources/views/inc/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-dark border rounded">
+    <div class="container">
+      <a class="navbar-brand text-success" href="#">LSAPP</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-white" aria-current="page" href="/">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="/about">ABOUT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="/services">SERVICES</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
---
+++ lsapp/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
+Route::resource('post', 'PostsController');
